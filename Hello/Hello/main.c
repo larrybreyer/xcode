b3da09b81450bdb3ebf6e326ada2a09ec64aeb36
Larry Breyer <larrybreyer@gmail.com>
2017-09-07 17:13:03 -0700

    daily tweaks

---
+++ Hello/Hello/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Hello
+//
+//  Created by Larry Breyer on 9/7/17.
+//  Copyright © 2017 Larry Breyer. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ test/hello.cpp
@@ -0,0 +1,14 @@
+// C++ "Hello World" using Xcode
+// vim: ai nu
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    cout << "Hello, World!\n";
+    cout << endl;  // Same as \n
+    return 0;
+    
+}
