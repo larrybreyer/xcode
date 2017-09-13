42be6c45203ab39434fd26afa248360bbacfc089
Larry Breyer <larrybreyer@gmail.com>
2017-09-12 21:08:41 -0700

    daily tweaks

---
+++ prog1/prog1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by Larry Breyer on 9/12/17.
+//  Copyright © 2017 Larry Breyer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Programming is fun!");
+    }
+    return 0;
+}
